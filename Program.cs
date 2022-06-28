25c1fa966cc4f87295bb0ee97efc3bbd214a6792
NGShpagin <shpagin9393@mail.ru>
2022-06-28 23:12:36 +0300

    add new method CountArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] CountArray(string[] array)
+
+{
+
+    int len = array.Length;
+    string[] resultArray = new string[len];
+    int index = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (array[i].Length < 4) 
+        {
+            resultArray[index] = array[i];
+            index++;
+        }
+    }
+    Array.Resize(ref resultArray, index);
+    return resultArray;
+}
+
+string[] myArr = {"1234", "1567", "-2", "computer science"};
