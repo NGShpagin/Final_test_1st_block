f14d6791f00a68a029ed3b53e372fa807e16d34f
NGShpagin <shpagin9393@mail.ru>
2022-06-28 23:16:04 +0300

    first var of decision is ready (with prefilled massive)

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 ﻿string[] CountArray(string[] array)
-
 {
-
     int len = array.Length;
     string[] resultArray = new string[len];
     int index = 0;
     for (int i = 0; i < len; i++)
     {
-        if (array[i].Length < 4) 
+        if (array[i].Length < 4)
         {
             resultArray[index] = array[i];
             index++;
@@ -17,4 +15,19 @@
     return resultArray;
 }
 
-string[] myArr = {"1234", "1567", "-2", "computer science"};
+
+
+string PrintArray(string[] array)
+{
+    string txt = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        txt += $"{array[i]}; ";
+    }
+    return $"[{txt.Remove(txt.Length - 2, 2)}]";
+}
+
+string[] myArr = { "1234", "1567", "-2", "computer science" };
+Console.WriteLine($"My array: {PrintArray(myArr)}");
+string[] resultArr = CountArray(myArr);
+Console.WriteLine($"My array: {PrintArray(resultArr)}");
